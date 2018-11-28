435856a54c7d35e2fa18acb0c16756fc74ac4e9c
Andrew <ajo14@duke.edu>
2018-11-28 18:28:38 -0500

    initial commit

---
+++ submissions/script.js
@@ -0,0 +1,89 @@
+var config = {
+    apiKey: "AIzaSyC4B3UpwTcv33YV8_mnUk7O90cQwhZd_CE",
+    authDomain: "visartssubmissions-67e5b.firebaseapp.com",
+    databaseURL: "https://visartssubmissions-67e5b.firebaseio.com",
+    projectId: "visartssubmissions-67e5b",
+    storageBucket: "visartssubmissions-67e5b.appspot.com",
+    messagingSenderId: "959959918208"
+};
+
+var db = firebase.initializeApp(config).database();
+var storageRef = firebase.storage().ref();
+var imagesRef = db.ref('images');
+
+Vue.use(VueFire);
+
+var app = new Vue({
+    el: "#input",
+    data: function(){
+        return {
+            firstName: null,
+            lastName: null,  //TODO: NEED TO ENSURE THERE ARE NO SPACES
+            dukeEmail: null,
+            yearAtDuke: null,
+            title: null,
+            medium: null,
+            yearCreated: null,
+            dimensions: null,
+            description: null,
+            photo: null,
+            hardcopy: null,
+            newImageTitle: ''
+        };
+    },
+    firebase: {
+        images: imagesRef
+    },
+    methods: {
+        addImage: function(title, url) {
+            // now that image has been stored in Firebase, create a reference to it in database
+            console.log("called");
+            imagesRef.push({
+                title: title,
+                url: url
+            });
+            // reset input values so user knows to input new data
+            this.newImageTitle = '';
+        },
+        storeImage: function(firstName, lastName, dukeEmail, yearAtDuke, title, medium, yearCreated, dimensions, description, hardcopy) {
+            // get input element user used to select local image
+            var input = document.getElementById('files');
+            // have all fields in the form been completed
+            if (input.files.length > 0) {
+                var file = input.files[0];
+                // get reference to a storage location and
+                var refLoc = storageRef.child('images/' + file.name);
+                storageRef.child('images/' + file.name)
+                          .put(file)
+                          .then(snapshot => {
+                               return snapshot.ref.getDownloadURL();   // Will return a promise with the download link
+                           })
+
+                           .then(downloadURL => {
+                                db.ref('/').push({
+                                    firstName: firstName,
+                                    lastName: lastName, 
+                                    dukeEmail: dukeEmail,
+                                    netID: dukeEmail.split("@")[0],
+                                    yearAtDuke: yearAtDuke,
+                                    title: title,
+                                    medium: medium,
+                                    yearCreated: yearCreated,
+                                    dimensions: dimensions,
+                                    description: description,
+                                    hardcopy: hardcopy,
+                                    votes: 0,
+                                    fileName: file.name,
+                                    url: downloadURL
+                                });
+                           })
+                           .catch(error => {
+                              console.log(`Failed to upload file and get link - ${error}`);
+                           });
+                // reset input values so user knows to input new data
+                input.value = '';
+            }
+        }
+    }
+})
+app.$mount('#input')
