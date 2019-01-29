ab64bfd96155c259e641efbe543e601c025518f6
Danny Wu <daniel3wu@gmail.com>
2019-01-28 22:19:38 -0500

    Added carousel

--- voting/script.js
+++ voting/script.js
@@ -1,3 +1,5 @@
+
+
 var config = {
     apiKey: "AIzaSyC4B3UpwTcv33YV8_mnUk7O90cQwhZd_CE",
     authDomain: "visartssubmissions-67e5b.firebaseapp.com",
@@ -12,47 +14,68 @@ var dbRef = db.ref();
 
 var childData = []
 var childDataCount = 0;
+var iter = -1;
 
 Vue.use(VueFire);
 
+// Required once to load the data from the database into childData array
+dbRef.once("value")
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                childData[childDataCount++] = childSnapshot.val();
+            });
+        }); 
 var app = new Vue({
     el: "#input",
     data: function(){
         return {
-            firstName: null,
-            lastName: null,  //TODO: NEED TO ENSURE THERE ARE NO SPACES
-            dukeEmail: null,
-            yearAtDuke: null,
-            title: null,
-            medium: null,
-            yearCreated: null,
-            dimensions: null,
-            description: null,
-            photo: null,
-            hardcopy: null,
+            firstName: "",
+            lastName: '',  //TODO: NEED TO ENSURE THERE ARE NO SPACES
+            dukeEmail: '',
+            yearAtDuke: '',
+            title: '',
+            medium: '',
+            yearCreated: '',
+            dimensions: '',
+            description: '',
+            photo: '',
+            hardcopy: '',
             newImageTitle: '',
-            childDataVue: childData
+            url: '',
         };
     },
     firebase: {
         artInfo: dbRef,
-        
     },
     methods: {
-        add: function() {
-            console.log(this.rating);
+        initializeSlides(){
+              document.getElementById('startButton').style.display = 'none';
+              document.getElementById('slideshow').style.display = 'block';
+              iter = 0;
+              this.firstName = childData[0].firstName;
+              this.lastName = childData[0].lastName;
+              this.title = childData[0].title;
+              this.url = childData[0].url;
+              this.description = childData[0].description;
+              this.medium = childData[0].medium;
+              this.dimensions = childData[0].dimensions;
         },
-        remove: function(){
-            console.log("Removed a vote");
-        },
-        main: function(){
-            dbRef.once("value")
-              .then(function(snapshot) {
-                snapshot.forEach(function(childSnapshot) {
-                  childData[childDataCount++] = childSnapshot.val();
-                });
-              });  
-              console.log(childData);
+        nextSlide(n){
+              iter = iter + n;
+              if(iter < 0){
+                iter = childData.length - 1;
+              }
+              if(iter >= childData.length){
+                iter = 0;
+              }
+              this.firstName = childData[iter].firstName;
+              this.lastName = childData[iter].lastName;
+              this.title = childData[iter].title;
+              this.url = childData[iter].url;
+              this.description = childData[iter].description;
+              this.medium = childData[iter].medium;
+              this.dimensions = childData[iter].dimensions;
+              
         },
     }
 })
